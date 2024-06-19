7c111cda86617fb287bb73fbece27b0d1f794c50
Manuel <manuel18dsa@gmail.com>
2024-06-19 14:13:18 -0400

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
