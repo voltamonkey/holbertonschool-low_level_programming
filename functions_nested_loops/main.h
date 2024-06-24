3052d2530aa3c2a647e523565a50b169a37bc72c
Manuel <manuel18dsa@gmail.com>
2024-06-24 16:30:50 -0400

    2-print_alphabet_x10.c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
@@ -8,3 +9,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+
