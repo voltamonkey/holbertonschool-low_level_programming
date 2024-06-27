f121ea1848d868641d1a8242686fc5854dca7a70
Manuel <manuel18dsa@gmail.com>
2024-06-27 16:02:54 -0400

    0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memset: fill the memory
+ *@s: memory to fill
+ *@b: constant byte to fill
+ *@n: bytes
+ *
+ * Return: Memory filled
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+	for (x = 0; x < n; x++)
+{
+	  s[x] = b;
+}	
+return (s)
+}	
