107cf6935ee2b893e4ca1a0c8c6e53f7fe2b0de2
Manuel <manuel18dsa@gmail.com>
2024-07-08 15:05:25 -0400

    1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog- initialize variable
+ *@d: point ti start
+ *@name: name of the dog
+ *@owner: owner of the dog 
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	if (d ! = NULL)
+	{
+		d-> name = name;
+		d-> age = age;
+		d-> owner = owner;
+
+	}
+}
+
