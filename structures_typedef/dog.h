b14556e66d954553bb1a33fb3f6ece9c83838f6c
Manuel <manuel18dsa@gmail.com>
2024-07-08 11:57:16 -0400

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  struct dog my_dog;
+
+   char *name;
+   float age;
+   char *owner;
+}
+int main (void)
+{
+  struct dog my_dog;
+
+  my_dog.name = "Tarek";
+  my_dog.age = 2.5;
+  my_dog.owner = "Monic";
+  printf("My name is %s, and I am %.1f :) - Woof!/n", my_dog.name, my_dog.age);
+  return (0)
+}
