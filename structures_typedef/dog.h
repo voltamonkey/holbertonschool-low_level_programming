fba0c8894654adee4c0d974cfcfcdaace6b73c3c
Manuel <manuel18dsa@gmail.com>
2024-07-08 13:26:41 -0400

    Update3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,25 +2,14 @@
 #include "dog.h"
 
 /**
- * main - check the code 
- *
- * Return: Always 0.
+ * struct dog - structure definition of a dog
+ * @name: character * Return: Always 0.
+ * @age: intenger
+ *@ouner: Character string
  */
-int main(void)
-{
-  struct dog my_dog;
-
+struct dog {
    char *name;
    float age;
    char *owner;
-}
-int main (void)
-{
-  struct dog my_dog;
 
-  my_dog.name = "Tarek";
-  my_dog.age = 2.5;
-  my_dog.owner = "Monic";
-  printf("My name is %s, and I am %.1f :) - Woof!/n", my_dog.name, my_dog.age);
-  return (0)
 }
