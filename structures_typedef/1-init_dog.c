e9e971fd23d2dafe38f1850ec854be0d25820c03
Manuel <manuel18dsa@gmail.com>
2024-07-08 15:26:35 -0400

    all

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -5,16 +5,16 @@
  * init_dog- initialize variable
  *@d: point ti start
  *@name: name of the dog
- *@owner: owner of the dog 
+ *@owner: owner of the dog
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		d-> name = name;
-		d-> age = age;
-		d-> owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 
 	}
 }
