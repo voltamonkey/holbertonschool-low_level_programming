9e4b2bc5b8a41f7493b96d1cf40b55a4a267b95b
Manuel <manuel18dsa@gmail.com>
2024-07-09 10:46:19 -0400

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,11 +7,23 @@
  * @age: intenger
  * @owner: Character string
 */
-struct dog
 
+struct dog
 {
 char *name;
 float age;
 char *owner;
 };
-#endif 
+
+/**
+ * typedef dog_t - define a new type of struct
+ */
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
+#endif
