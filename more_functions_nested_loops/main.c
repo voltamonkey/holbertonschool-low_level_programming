a81d65d55e95887f61677633b0e6ac74e5e2c980
Manuel <manuel18dsa@gmail.com>
2024-07-17 06:33:09 -0400

    main.c

---
+++ more_functions_nested_loops/main.c
@@ -0,0 +1,24 @@
+julien@ubuntu:~/$ cat 0-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
+}
+julien@ubuntu:~/$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-isupper.c -o 0-isuper
+julien@ubuntu:~/$ ./0-isuper 
+A: 1
+a: 0
+julien@ubuntu:~/$ 
